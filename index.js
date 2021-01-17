2120b59821c3d2726bda99d685a57e7bce801949
cicciodist <fstefano@zinio.com>
2021-01-17 11:39:27 +0100

    Initial mockup

--- index.js
+++ index.js
@@ -10,12 +10,18 @@ app.engine(
   "handlebars",
   handlebars({
     layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
   })
 );
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
   res.render("main", { layout: "index" });
+});
+
+app.get("/users", (req, res) => {
+  //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+  res.render("users", { layout: "index" });
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
